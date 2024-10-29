560402988b00a8041fa553f7f1d948967579e6de
Aaron Brown <abrown52@tamu.edu>
2024-10-29 17:05:27 -0500

    Finished LRMultiClass drafted code

--- src/LRMultiClass.cpp
+++ src/LRMultiClass.cpp
@@ -61,7 +61,7 @@ double sum_diag(const arma::mat& main_mat, int num_col){
 // numIter - number of iterations, default 50
 // eta - damping parameter, default 0.1
 // lambda - ridge parameter, default 1
-// beta_init - p x K matrix of starting beta values (always supplied in right format)
+// // beta_init - p x K matrix of starting beta values (always supplied in right format)
 Rcpp::List LRMultiClass_c(const arma::mat& X, const arma::uvec& y, const arma::mat& beta_init,
                                int numIter = 50, double eta = 0.1, double lambda = 1){
     // All input is assumed to be correct
@@ -111,10 +111,53 @@ Rcpp::List LRMultiClass_c(const arma::mat& X, const arma::uvec& y, const arma::m
     objective[0] = objective_obj;
 
 
-
+    // ////
     // Newton's method cycle - implement the update EXACTLY numIter iterations
-
-
+    // ////
+    // Initialize Terms:
+    arma::mat X_tran = X.t();
+    arma::mat Identity = arma::eye<arma::mat>(p, p);
+    
+    // Loop: Within one iteration perform the update, calculate updated objective function and training/testing errors in %
+    // ////
+    for(int i = 0; i < numIter; i++){
+      
+      
+      for(int k = 0; k < K; k++){
+        
+        // W term configuration (in Hessian)
+        //  W in this case is the kth diagonal element of main W mat
+        arma::mat W = p_k.col(k) % (1 - p_k.col(k));
+        
+        // Generating Function Update:
+        arma::mat g = X_tran * (p_k.col(k) - y_indicator.col(k)) + (lambda * beta.col(k));
+        // Hessian Update:
+        arma::mat h = ((X % W) * X) + (lambda * Identity);
+        // Damped Newton's Update:
+        beta.col(k) = beta.col(k) - (eta * (h.i() * g));
+        
+      }
+      
+      // Update pk
+      // Num:
+      arma::mat Xb = X * beta;
+      arma::mat exp_Xb = arma::exp(Xb);
+      // Denom:
+      arma::colvec sum_exp_Xb = arma::sum(exp_Xb, 1);
+      // pk:
+      arma::mat p_k = exp_Xb / sum_exp_Xb;
+      
+      // Compute Objective Value f(beta):
+      arma::mat log_pk = arma::log(p_k);
+      double neg_log_lik = -(arma::accu(y_indicator * log_pk)); // Negative Log Likelihood
+      double ridge_reg = (lambda / 2) * (arma::accu(arma::sum(arma::square(beta), 0))); // Ridge Penalty
+      double objective_obj = neg_log_lik + ridge_reg; // Objective Value
+      
+      objective[i + 1] = objective_obj; // Append
+      
+    }
+    
+    
     // Create named list with betas and objective values
     return Rcpp::List::create(Rcpp::Named("beta") = beta,
                               Rcpp::Named("objective") = objective);
